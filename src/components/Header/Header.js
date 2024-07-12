77eba6f682a65750ce773381d152d031457eaabe
chiba_chan <84514511+chibachann@users.noreply.github.com>
2024-07-12 18:40:01 +0900

    aaa

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,6 +9,16 @@ const Header = () => {
         setIsOpen(!isOpen);
     };
 
+    const menuItems = [
+        { text: "三気堂について", href: "/" },
+        { text: "三気堂の取り組み", href: "/" },
+        { text: "店舗情報", href: "/" },
+        { text: "採⽤情報", href: "/" },
+        { text: "お知らせ", href: "/" },
+        { text: "コラム", href: "/" },
+        { text: "お問い合わせ", href: "/" }
+    ];
+
     return (
         <header className={styles.header}>
             <div className={styles.container}>
@@ -21,17 +31,23 @@ const Header = () => {
                 <div className={`${styles.sideMenu} ${isOpen ? styles.open : ''}`}>
                     <nav className={styles.navigation}>
                         <ul>
-                            <li><a href="/">ホーム</a></li>
-                            <li><a href="/">研究テーマ</a></li>
-                            <li><a href="/">活動紹介</a></li>
-                            <li><a href="/">論文・講演等</a></li>
-                            <li><a href="/">メンバー紹介</a></li>
-                            <li><a href="/">トピックス</a></li>
-                            <li><a href="/">お問い合わせ</a></li>
+                            {menuItems.map((item, index) => (
+                                <li key={index}>
+                                    <a href={item.href}>{item.text}</a>
+                                </li>
+                            ))}
                         </ul>
-
                     </nav>
-                    
+                </div>
+
+                <div className={styles.listMenu}>
+                    <ul>
+                    {menuItems.map((item, index) => (
+                                    <li key={index}>
+                                        <a href={item.href}>{item.text}</a>
+                                    </li>
+                                ))}
+                    </ul>
                 </div>
             </div>
         </header>
